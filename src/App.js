efc5bf79236fd8bb2d8fd20e99c3e06e14a590ec
developedbyumair <umairsadaqat@invotyx.co.uk>
2023-07-20 23:11:55 +0500

    Fix: Comments

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
     <div className="App">
       <React.Suspense fallback={<GetLoader />}>
         <Routes>
-          {/*  */}
           <Route index path="/*" element={<User />} />
         </Routes>
       </React.Suspense>
